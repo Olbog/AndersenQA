ecf89dadf6fd5d55cd8fda8373105997d65faf4e
Olbog <olbog2009@bk.ru>
2022-04-08 16:36:08 +0300

    DZ_10

---
+++ src/test/java/com/andersen/LoginPage.java
@@ -0,0 +1,66 @@
+package com.andersen;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+    /**
+     * конструктор класса, занимающийся инициализацией полей класса
+     */
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
+
+    /**
+     * определение локатора поля ввода логина
+     */
+    @FindBy(xpath = "//*[@id='signupform-username']")
+    private WebElement loginField;
+
+    /**
+     * определение локатора кнопки зарегистрироваться
+     */
+    @FindBy(xpath = "//*[@id=\"signup_btn\"]")
+    private WebElement loginBtn;
+
+    /**
+     * определение локатора поля email
+     */
+    @FindBy(xpath = "//*[@id=\"signupform-email\"]")
+    private WebElement emailField;
+
+    /**
+     * определение локатора поля ввода пароля
+     */
+    @FindBy(xpath = "//*[@id=\"signupform-password\"]")
+    private WebElement passwdField;
+
+    /**
+     * метод для ввода логина
+     */
+    public void inputLogin(String login) {
+        loginField.sendKeys(login); }
+
+    /**
+     * метод для ввода email
+     */
+    public void inputEmail(String login) {
+        emailField.sendKeys(login); }
+
+    /**
+     * метод для нажатия кнопки пароля
+     */
+    public void inputPasswd() {
+        passwdField.sendKeys(); }
+    /**
+     * метод для осуществления нажатия кнопки входа в аккаунт
+     */
+    public void clickLoginBtn() {
+        loginBtn.click(); }
+
+
+}
--- src/test/java/com/andersen/LoginTest.java
+++ src/test/java/com/andersen/LoginTest.java
@@ -1,4 +1,37 @@
 package com.andersen;
 
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.AfterClass;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import java.util.concurrent.TimeUnit;
+
 public class LoginTest {
+    public static LoginPage loginPage;
+    public static WebDriver driver;
+
+
+    @BeforeClass
+    public static void setup(){
+        System.setProperties("webdriver.chrome.driver", ConfProperties.getProperty ("chromedriver"));
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
+        driver.get("https://diary.ru/user/registration");
+        driver.findElement(By.id("signupform- email")).sendKeys( "testLogin@test.com");
+        driver.findElement(By.id("signup_btn")).click();
+        loginPage = new LoginPage(driver);
+        driver = new ChromeDriver();
+    }
+    @Test
+    public void loginTest() {
+        loginPage.inputLogin(ConfProperties.getProperty("login"));
+        loginPage.clickLoginBtn();
+        loginPage.inputPasswd(ConfProperties.getProperty("password"));
+        loginPage.clickLoginBtn();
+    }
 }
