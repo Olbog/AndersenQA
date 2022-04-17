f3a03c9adce3c874ce6b8e5009e747be25ef1fd9
Olbog <olbog2009@bk.ru>
2022-04-17 14:52:47 +0300

    Lesson_13 draft 2

--- src/test/java/com/andersen/lesson10/LoginTest.java
+++ src/test/java/com/andersen/lesson10/LoginTest.java
@@ -1,11 +1,9 @@
-package com.andersen;
+package com.andersen.lesson10;
 
 import org.junit.Assert;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.AfterClass;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.concurrent.TimeUnit;
