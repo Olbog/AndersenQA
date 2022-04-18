ba892aa20788e9aa59308ee5af4dd892e28eb75c
Olbog <olbog2009@bk.ru>
2022-04-18 11:11:38 +0300

    Lesson_12 draft

--- src/test/java/com/andersen/lesson12/FunctionTriangleTest.java
+++ src/test/java/com/andersen/lesson12/FunctionTriangleTest.java
@@ -1,17 +1,16 @@
 package com.andersen.lesson12;
 
 import lesson12.FunctionTriangle;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
-public class FunctionTriangleTest {
+class FunctionTriangleTest {
 
-    @Test
-    public void rez() {
+    @org.junit.jupiter.api.Test
+    void rez() {
         FunctionTriangle functionTriangle = new FunctionTriangle();
         double actual = functionTriangle.rez();
         double expected = 26.832815729997478;
         assertEquals(actual, expected, 100);
-
     }
 }
