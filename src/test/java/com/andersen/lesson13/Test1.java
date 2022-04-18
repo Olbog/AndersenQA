ef3db42f5d0b66b44eb5cbb49b1f849ddf294c5f
Olbog <olbog2009@bk.ru>
2022-04-18 11:46:41 +0300

    Lesson_13 draft

--- src/test/java/com/andersen/lesson13/Test1.java
+++ src/test/java/com/andersen/lesson13/Test1.java
@@ -1,13 +1,25 @@
 package com.andersen.lesson13;
 
-import com.andersen.lesson10.ConfProperties;
+import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import java.time.Duration;
 
 
 public class Test1 {
+    @Test
+    public void ftiGroup(){
+        System.setProperty("webdriver.chrome.driver", "C:/Windows/System32/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://andersenlab.com/");
+        WebElement ftiGroup = driver.findElement(By.xpath("ftiGroupElement"));
+        ftiGroup.click();
+
+
+    }
 
 
 }
